1049d0ebfea4104782aef8067e5a39ce07923c7d
binja <buzzzzx233@gmail.com>
2020-05-22 13:50:32 +0800

    convert Pet.js App.js to .jsx, config Eslint for React

--- src/App.js
+++ src/App.js
@@ -4,29 +4,38 @@ import Pet from "./Pet";
 
 //App component.
 const App = () => {
-  return React.createElement(
-    "div", // the tag we want to create, also can be a component like App
-    {}, // the attribute we want to put inside in this tag, e.g. {id: "app-div"}
-    [
-      // child element of this tag, also can be an array of child elements.
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Shanshan",
-        animal: "Cat",
-        breed: "Go",
-      }),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Dokin",
-        animal: "Bird",
-        breed: "Mixed",
-      }),
-    ]
+  // return React.createElement(
+  //   "div", // the tag we want to create, also can be a component like App
+  //   {}, // the attribute we want to put inside in this tag, e.g. {id: "app-div"}
+  //   [
+  //     // child element of this tag, also can be an array of child elements.
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, {
+  //       name: "Shanshan",
+  //       animal: "Cat",
+  //       breed: "Go",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Luna",
+  //       animal: "Dog",
+  //       breed: "Havanese",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Dokin",
+  //       animal: "Bird",
+  //       breed: "Mixed",
+  //     }),
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Shanshan" animal="Cat" breed="Mixed" />
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Dokin" animal="Bird" breed="Worm" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -2,9 +2,19 @@ import React from "react";
 // Pet component.
 export default function Pet({ name, animal, breed }) {
   // Destructuring.
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, name),
+  //   React.createElement("h2", {}, animal),
+  //   React.createElement("h2", {}, breed),
+  // ]);
+
+  // This is jsx, will automatically translate to above code(createElement...)
+  // Which is through parce-babel
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
