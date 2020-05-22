86ca7fd86aaf7847e431c2ea7323b6e5a49e7b38
binja <buzzzzx233@gmail.com>
2020-05-22 19:56:28 +0800

    useEffect and rules to control update

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "cat", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]); // every time after render or update, first clean out placeholder
+    setBreed(""); // every time after render or update, first clean out placeholder
+
+    pet.breeds(animal).then(({ breeds }) => {
+      setBreeds(breeds.map(({ name }) => name));
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // control that only when update animal or setBreed or setBreeds, we use effect.
 
   return (
     <div className="search-params">
