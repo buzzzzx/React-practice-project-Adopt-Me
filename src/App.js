c3658c4ea26d7998f5988359b4e478201c23d3f0
binja <buzzzzx233@gmail.com>
2020-05-22 18:18:30 +0800

    useState, custome hooks, update ESLint, bring in petfinder client

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 //App component.
 const App = () => {
-  // return React.createElement(
-  //   "div", // the tag we want to create, also can be a component like App
-  //   {}, // the attribute we want to put inside in this tag, e.g. {id: "app-div"}
-  //   [
-  //     // child element of this tag, also can be an array of child elements.
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Shanshan",
-  //       animal: "Cat",
-  //       breed: "Go",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Luna",
-  //       animal: "Dog",
-  //       breed: "Havanese",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Dokin",
-  //       animal: "Bird",
-  //       breed: "Mixed",
-  //     }),
-  //   ]
-  // );
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Shanshan" animal="Cat" breed="Mixed" />
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Dokin" animal="Bird" breed="Worm" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "cat", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
