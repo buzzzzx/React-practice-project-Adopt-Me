4ab734d41625e30823b951de785119967ba34ef4
Anak1n <buzzzzx233@gmail.com>
2020-06-19 13:01:49 +0800

    testing: api mocks

---
+++ __mocks__/frontendmasters/pet.js
@@ -0,0 +1,43 @@
+import { readSyncFile } from "fs";
+import path from "path";
+import { act } from "@testing-library/react";
+
+const breeds = [
+  { name: "Bichon Frise" },
+  { name: "Bolognese" },
+  { name: "Bolonka" },
+  { name: "Coton de Tulear" },
+  { name: "Havanese" },
+  { name: "Lowchen" },
+  { name: "Maltese" },
+];
+
+const doggos = JSON.parse(
+  readSyncFile(path.join(__dirname, "/res.json")).toString()
+);
+
+export const ANIMALS = ["dog", "cat", "bird"];
+export const _breeds = breeds;
+export const _dogs = doggos.animals;
+
+const mock = {
+  breeds: jest.fn(() => {
+    return {
+      then: (callback) =>
+        act(() => {
+          callback({ breeds });
+        }),
+    };
+  }),
+  animals: jest.fn(() => {
+    return {
+      then: (callback) => {
+        act(() => {
+          callback({ doggos });
+        });
+      },
+    };
+  }),
+};
+
+export default mock;
